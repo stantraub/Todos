5f2feecedbba3987819f864a91bd8660b84004ba
Stanley <stanley.traub@gmail.com>
2019-12-15 12:30:54 -0800

    Fixed webpack

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const root = document.getElementById('content');
+    // ReactDOM.render(<Root store={store} />, root)
+})
